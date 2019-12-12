179e18c776b802b8e711e406b47f7c8d375e8730
Michael Ciocca <mciocca89@gmail.com>
2019-12-11 20:10:08 -0500

    stub for dasboard component

---
+++ src/containers/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export default class Dasboard extends Component<RouteComponentProps> {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
