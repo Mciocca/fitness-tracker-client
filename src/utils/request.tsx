39f37c3792186984acea0e17d73a08c5f4e8902f
Michael Ciocca <mciocca89@gmail.com>
2019-12-26 21:01:29 -0500

    add get to Request service,  add csrf support

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,20 +1,43 @@
+
+const JSON_ERROR = { errors: ["We've experienced a problem, please try again"] }
 export default class Request {
+  public static async get(url: string): Promise<any> {
+    const response = await fetch(url, { credentials: 'include' });
+
+    try {
+      const data = await response.json();
+      return handleResponse(response, data);
+    } catch {
+      return Promise.reject(JSON_ERROR)
+    }
+  }
+
   public static async post(url: string, body: Object): Promise<any> {
     const response = await fetch(url, {
+      credentials: 'include',
       method: 'POST',
       headers: {
         'Connection': 'keep-alive',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify({ ...body, _csrf: csrfToken() })
     });
 
     try {
       const data = await response.json();
       return handleResponse(response, data);
     } catch {
-      return Promise.reject({ errors: ["We've experienced a problem, please try again"]});
+      return Promise.reject(JSON_ERROR);
     }
+  }
+}
+
+const csrfToken = (): string | undefined => {
+  const cookies = decodeURIComponent(document.cookie).split(';');
+  const token = cookies.find(cookie => cookie.includes('_csrf'));
+
+  if (token) {
+    return token.split('=')[1]
   }
 }
 
