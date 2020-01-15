8ae68af2a7e9f050f786b90b5feaf522b9eb9b1d
Michael Ciocca <mciocca89@gmail.com>
2020-01-14 19:15:17 -0500

    Fix issue with MaterialUI and Jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,13 @@
 import { GlobalWithFetchMock } from "jest-fetch-mock";
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import React from "react"
 
 configure({ adapter: new Adapter() });
 
 const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
 customGlobal.fetch = require('jest-fetch-mock');
 customGlobal.fetchMock = customGlobal.fetch;
+
+//this fixes an issue with material ui in jest tests
+React.useLayoutEffect = React.useEffect
