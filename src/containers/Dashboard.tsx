f4f0d912b641a45ce97badb63ac9e3e4b1eb2bb1
Michael Ciocca <mciocca89@gmail.com>
2020-01-14 19:16:16 -0500

    add Notification component to dashboard

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -3,22 +3,34 @@ import NavBar from '../components/NavBar';
 import { withRouter, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import Loading from '../components/Loading';
 import { Store } from '../reducers/types';
+import { hideNotification } from '../actions/uiActions';
 import { connect, ConnectedProps } from 'react-redux';
 import Profile from './Profile';
+import Notification from '../components/common/Notification';
 
 const mapStateToProps = (state: Store) => ({
   user: state.user,
-  showLoading: state.ui.showLoading
+  showLoading: state.ui.showLoading,
+  notification: { ...state.ui.notification, showNotification: state.ui.showNotification }
 });
 
-const connector = connect(mapStateToProps);
+const mapDispatchToProps = {
+  hideNotification
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>
 type Props = PropsFromRedux & RouteComponentProps
 
-const Dashboard: React.FC<Props> = ({ user, showLoading }) => {
+const Dashboard: React.FC<Props> = ({ user, showLoading, notification, hideNotification }) => {
   return (
     <div>
       <NavBar />
+      <Notification
+        hideNotification={hideNotification}
+        message={notification.message}
+        severity={notification.severity}
+        open={notification.showNotification} />
       <Loading showLoading={showLoading} />
       <Switch>
         <Route path="/dashboard/profile">
