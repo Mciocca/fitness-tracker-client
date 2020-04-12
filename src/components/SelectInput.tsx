38b3ebc7b595e9f5623907011683529887babbad
Michael Ciocca <mciocca89@gmail.com>
2020-04-12 18:22:41 -0400

    add styles to selecInput

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -4,23 +4,24 @@ import { MenuItem, FormControl, InputLabel, Select } from '@material-ui/core';
 interface SelectInputProps {
   onChange: (event: React.ChangeEvent<any>) => void,
   className?: string,
-  selectorId: string,
+  styles?: object
   defaultValue?: string
+  selectorId: string,
   value: string,
   options: string[],
   name: string,
-  label: string
+  label: string,
 }
 
 const SelectInput: React.FC<SelectInputProps> =
-({ options, onChange, className, selectorId, defaultValue, value, name, label }) => {
+({ styles, options, onChange, className, selectorId, defaultValue, value, name, label }) => {
   const optionItems = () => {
     return options.map((value: string, index: number) => <MenuItem value={value} key={index}>{value}</MenuItem>);
   }
 
   return (
     <>
-      <FormControl className={className}>
+      <FormControl style={styles} className={className}>
         <InputLabel id={selectorId}>{label}</InputLabel>
         <Select labelId={selectorId} onChange={onChange} name={name} defaultValue={defaultValue} value={value}>
           {optionItems()}
