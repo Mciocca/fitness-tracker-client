b81845426d3979712a3c7972b5c4c3232c012760
Michael Ciocca <mciocca89@gmail.com>
2020-05-06 21:26:30 -0400

    create WorkOut Form component

---
+++ src/test_helpers/fixtures/UserFixture.tsx
@@ -0,0 +1,19 @@
+export default {
+  email: 'admin@user.com',
+  firstName: 'admin',
+  id: 95,
+  lastName: 'user',
+  name: 'admin user',
+  profile: {
+    age: 31,
+    gender: 'Male',
+    goal: 'Maintain weight',
+    height: 69,
+    options: {
+      gender: ['Male', 'Female'],
+      goals: ['Maintain weight', 'Gain weight', 'Lose weight'],
+    },
+    startingWeight: 155,
+  },
+  updateUrl: '/api/v1/user',
+};
