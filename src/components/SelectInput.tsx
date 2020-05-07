d36f86ba96fb6d5aca794e30558f419d7ab91a97
Michael Ciocca <mciocca89@gmail.com>
2020-05-06 21:27:10 -0400

    add missing htmlFor to SelectInput component

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -2,33 +2,54 @@ import React from 'react';
 import { MenuItem, FormControl, InputLabel, Select } from '@material-ui/core';
 
 interface SelectInputProps {
-  onChange: (event: React.ChangeEvent<any>) => void,
-  className?: string,
-  styles?: object
-  defaultValue?: string
-  selectorId: string,
-  value: string,
-  options: string[],
-  name: string,
-  label: string,
+  onChange: (event: React.ChangeEvent<any>) => void;
+  className?: string;
+  styles?: object;
+  defaultValue?: string;
+  selectorId: string;
+  value: string;
+  options: string[];
+  name: string;
+  label: string;
 }
 
-const SelectInput: React.FC<SelectInputProps> =
-({ styles, options, onChange, className, selectorId, defaultValue, value, name, label }) => {
+const SelectInput: React.FC<SelectInputProps> = ({
+  styles,
+  options,
+  onChange,
+  className,
+  selectorId,
+  defaultValue,
+  value,
+  name,
+  label,
+}) => {
   const optionItems = () => {
-    return options.map((value: string, index: number) => <MenuItem value={value} key={index}>{value}</MenuItem>);
-  }
+    return options.map((value: string, index: number) => (
+      <MenuItem value={value} key={index}>
+        {value}
+      </MenuItem>
+    ));
+  };
 
   return (
     <>
       <FormControl style={styles} className={className}>
-        <InputLabel id={selectorId}>{label}</InputLabel>
-        <Select labelId={selectorId} onChange={onChange} name={name} defaultValue={defaultValue} value={value}>
+        <InputLabel htmlFor={selectorId} id={selectorId}>
+          {label}
+        </InputLabel>
+        <Select
+          labelId={selectorId}
+          onChange={onChange}
+          name={name}
+          defaultValue={defaultValue}
+          value={value}
+        >
           {optionItems()}
         </Select>
       </FormControl>
     </>
   );
-}
+};
 
 export default SelectInput;
